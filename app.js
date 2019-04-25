e97758c17378643c97100b72af5ef58ed2080e33
Aamir Malik <aamir22188@Aamirs-MacBook-Pro.local>
2019-04-25 12:25:05 -0500

    additional updates

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ $().ready(function(){
         var pageId = $(this).attr("data-page");
         $("html, body").animate({ scrollTop: $("#"+pageId).offset().top }, 1000);
         console.log("work?")
-    
+
+
         
       });
     });
