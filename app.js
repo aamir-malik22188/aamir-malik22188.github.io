84eaad70bd3b3a8e586ed5e577dfb83f04d22917
Aamir Malik <aamir22188@gmail.com>
2019-09-21 14:27:25 -0500

    new push

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-$().ready(function(){
-    $("a").click(function(){
-        var pageId = $(this).attr("data-page");
-        $("html, body").animate({ scrollTop: $("#"+pageId).offset().top }, 1000);
-        console.log("work?")
 
-
-      });
-      $('.sidenav').sidenav();
-    });
-   
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+      
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
