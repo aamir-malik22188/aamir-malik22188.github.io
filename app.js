207b2a24f8ba190472dc1c3bf9e365c6857bdb3b
Aamir Malik <aamir22188@gmail.com>
2020-10-21 09:30:00 -0500

    new update

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-const next = document.querySelector("#next");
-const previous = document.querySelector("#previous");
-
-function handleScrollNext (direction) {
-    const cards = document.querySelector('.con-cards')
-    cards.scrollLeft = cards.scrollLeft += window.innerWidth /2 > 600 ? window.innerWidth / 2 : window.innerWidth -100
-}
-
-function handleScrollPrevious (direction) {
-    const cards = document.querySelector('.con-cards')
-    cards.scrollLeft = cards.scrollLeft += window.innerWidth /2 > 600 ? window.innerWidth / 2 : window.innerWidth -100
-}
-
-next.addEventListener('click', handleScrollNext);
-previous.addEventListener('click', handleScrollPrevious);
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
