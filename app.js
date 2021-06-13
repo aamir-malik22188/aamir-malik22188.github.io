0928fdf48ca6beaae2d7f26bad2433312adb5f08
Aamir Malik <aamir22188@gmail.com>
2021-06-13 10:14:24 -0500

    portfolio redesign

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems);
-});
+  // Scroll function for the Navbar
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 500) {
+      document.getElementById("navbar").style.top = "0px";
+      
+    } else {
+      document.getElementById("navbar").style.top = "-100px";
+    }
+  }
+
+  //Enable SideNav 
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+
+
+
+
